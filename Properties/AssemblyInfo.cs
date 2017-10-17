78e29a87fb0d63a59d99813c18005746c0bc7e2b
Steve05 <steve.yeh05@gmail.com>
2017-10-17 14:14:47 +0800

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BandNameGenerator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BandNameGenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1237b5ae-d05b-4431-8880-74dc79bdca4f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BandNameGenerator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+       
+        [TestMethod]
+        public void input_knife_should_return_TheKnife()
+        {
+            AssertBandNameGeneratorShouldBe("The Knife", "knife");
+        }
+
+        [TestMethod]
+        public void input_tart_should_return_TheKnife()
+        {
+            AssertBandNameGeneratorShouldBe("Tartart", "tart");
+        }
+
+        [TestMethod]
+        public void input_sandles_should_return_Sandlesandles()
+        {
+            AssertBandNameGeneratorShouldBe("Sandlesandles", "sandles");
+        }
+
+        [TestMethod]
+        public void input_bed_should_return_TheBed()
+        {
+            AssertBandNameGeneratorShouldBe("The Bed", "bed");
+        }
+
+        private static void AssertBandNameGeneratorShouldBe(string expected, string str)
+        {
+            var kata = new Kata();
+            var actual = kata.BandNameGenerator(str);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public string BandNameGenerator(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return "";
+            }
+
+            return str.First() == str.Last() ? CapitalizeFirstLetterOfString(str) + str.Substring(1) : "The " + CapitalizeFirstLetterOfString(str);
+        }
+
+        private string CapitalizeFirstLetterOfString(string str)
+        {
+            return str.ToUpper().First() + str.Substring(1) ;
+        }
+
+    }
+}
